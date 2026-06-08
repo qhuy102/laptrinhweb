7a5af3f3285c42d1174d33744964c56da9ec618f
Camloc123 <camloc0109@gmail.com>
2026-06-08 21:12:50 +0700

    Lưu dự án nhà thuốc và bỏ qua file rác VS

--- Web Quản Lí Nhà Thuốc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_Quản_Lí_Nhà_Thuốc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Web Quản Lí Nhà Thuốc/Models/HoaDon.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Web_Quản_Lí_Nhà_Thuốc.Models
-{
-    public class HoaDon
-    {
-    }
-}
